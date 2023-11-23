3838bfa0bd4e4354e3c14f11258ea4b02e328484
Abhijeet Sawale <abhijeetsawale@quantumphinance.com>
2023-11-23 12:50:08 +0530

    Add project files.

---
+++ script.js
@@ -0,0 +1,24 @@
+﻿
+function authenticate() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const encodedCredentials = btoa(username + ":" + password);
+
+    fetch("LoginDetails.txt")
+        .then(response => response.text())
+        .then(data => {
+            const lines = data.split("\n");
+          
+
+            for (var user of lines) {
+                user = user.replace(/\r/g, '');
+                
+                if (btoa(user) === encodedCredentials) {
+                    document.getElementById("message").innerHTML = "Authentication Successful";
+                    return;
+                }
+            }
+            document.getElementById("message").innerHTML = "Authentication Failed";
+        });
+}
